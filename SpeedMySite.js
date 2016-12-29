49cf3ed02b454a55153315d7ba7cdc34179f40a8
sjfricke <sjfricke@wisc.edu>
2016-12-28 21:35:12 -0600

    styling up file

--- SpeedMySite.js
+++ SpeedMySite.js
@@ -1,10 +1,10 @@
 var Nightmare = require('nightmare'); //used to run the headless browser
 var nightmare = Nightmare({ show: false }); //default is true
-var image_process = require("./helper_functions/image_process");
-var sanitize = require("sanitize-filename");
-var fs = require('fs-extra');
+var image_process = require("./helper_functions/image_process"); //set of image processing functions
+var sanitize = require("sanitize-filename"); //used to make sure file names are correct
+var fs = require('fs-extra'); //used to make directory checking eaiser
 
-var image_count = 0;
+var image_count = 0; //used to count images 
 
 var argv = require('minimist')(process.argv.slice(3)); //used for easy param parsing
 
@@ -27,12 +27,8 @@ var known_black_list = image_process.blackList();
 if (process.argv[2].substring(0,7) != "http://") process.argv[2] = "http://" + process.argv[2];
 
 nightmare
-    //  .goto('http://spencerfricke.com/webgl/')  
-//    .goto('http://stackoverflow.com/questions/8641596/how-to-get-all-images-from-img-tag-and-background-images-from-html-page-using-jq')  
     .goto(process.argv[2])  
     .inject('js', 'files/jquery.min.js') //TODO, not force pages with jQuery to load
-    //Work around to check if jquery is there or not
-    //.evaluate(function () {if (typeof jQuery != 'undefined') { return jQuery.fn.jquery; } else {  return  jQuery.fn.jquery; }})
     .evaluate(function(known_black_list){
         var all_images = [];
             
@@ -73,18 +69,16 @@ nightmare
                     
                     all_images.push(temp_object);
                 }
-            }
-            
-            //dont push to all_image each time as most of the * are not images
-            
+            }            
+            //dont push to all_image each time as most of the * are not images            
         });       
            
-        
         return all_images;
     
     }, known_black_list)
     .end()
     .then(function (result) {
+    
         console.log(result.length + " images found\n");
     
         var directory = (argv.o) ? (argv.o + "/old") : "SpeedMySite_Results/old/";
@@ -100,9 +94,9 @@ nightmare
                               
                 var img_name = directory + sanitize(result[i].src.substring(result[i].src.lastIndexOf("/") + 1));
                 
-                image_process.download(result[i].src, img_name, function(){
-                    console.log("image saved!");
+                image_process.download(result[i].src, img_name, function(){                    
                     image_count++;
+                    console.log("image saved! \t" + image_count + " of " + result.length);
                     
                     if (image_count == result.length) {
                         image_process.checkSize();
@@ -114,4 +108,8 @@ nightmare
     })
     .catch(function (error) {
         console.error('Search failed:', error);
-    });
+    }); //end of Nightmare
+
+
+//Work around to check if jquery is there or not
+//.evaluate(function () {if (typeof jQuery != 'undefined') { return jQuery.fn.jquery; } else {  return  jQuery.fn.jquery; }})
